1492cfd4a67247c920adbc61e5062dfeef3f7e39
Adrian Georgita <adrian.georgita@student.tuiasi.ro>
2024-03-26 13:49:50 +0200

    Made it read and send the content of the 'continut' files

--- server_web/server_web.py
+++ server_web/server_web.py
@@ -1,4 +1,6 @@
 import socket
+import os.path
+import stat
 # creeaza un server socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # specifica ca serverul va rula pe portul 5678, accesibil de pe orice ip al serverului
@@ -27,10 +29,63 @@ while True:
     print('S-a terminat cititrea.')
     # TODO interpretarea sirului de caractere `linieDeStart` pentru a extrage numele resursei cerute
     numeResursa = linieDeStart.split()[1]
-    print(numeResursa)
-    continut = "Hello World! - "+numeResursa
-    raspuns = "HTTP/1.1 200 OK\r\nContent-Length: 27\r\nContent-Type: text/plain;charset=utf-8\r\nServer: MyFootballServer\r\n\r\n"+continut
+
+    if numeResursa == "favicon.ico":
+        numeResursa = "/continut/favicon.ico"
+
+    resourceAvailable = False
+    if os.path.exists(".."+numeResursa):
+        resourceAvailable = True
+    #print(numeResursa)
+    startLine = "HTTP/1.1 404 Not Found\r\n"
+    content = "\r\n"
+    contentLength = str(len(content.encode('utf-8')))+"\r\n"
+    contentType = "text/html;charset=utf-8\r\n"
+    server = "MyFootballServer\r\n\r\n"
+    if resourceAvailable:
+        startLine = "HTTP/1.1 200 OK\r\n"
+        file = open(".."+numeResursa,"rb")
+        #print(file.readlines())
+        content = ""
+        #for line in file.readlines():
+         #   content = content + line
+        #print(content)
+        contentLength = str(os.stat(".."+numeResursa).st_size)+"\r\n"  #str(len(content.encode('utf-8')))+"\r\n"
+        #print(contentLength)
+        type = numeResursa.split(".")[1]
+        if type == "html":
+            contentType = "text/html"
+        elif type == "css":
+            contentType = "text/css"
+        elif type == "js":
+            contentType = "application/js"
+        elif type == "png":
+            contentType = "text/png"
+        elif type == "jpg" or type == "jpeg":
+            contentType = "text/jpeg"
+        elif type == "gif":
+            contentType = "text/gif"
+        elif type == "ico":
+            contentType = "image/x-icon"
+        else:
+            contentType = "text/plain"
+        contentType = contentType + "\r\n"
+    response = startLine+contentLength+contentType+server
+    print(response)
+    #response = startLine+"Content-Length: 27\r\nContent-Type: text/plain;charset=utf-8\r\nServer: MyFootballServer\r\n\r\n"+content
     # TODO trimiterea răspunsului HTTP
-    clientsocket.sendall(raspuns.encode("utf-8"))
+    clientsocket.sendall(response.encode("utf-8"))
+
+    print(response)
+
+    if resourceAvailable:
+        buffer = file.read(1024)
+        while(buffer):
+            print(buffer)
+            clientsocket.send(buffer)
+            buffer = file.read(1024)
+
+    clientsocket.sendall(b'\r\n')
+
     clientsocket.close()
     print('S-a terminat comunicarea cu clientul.')
